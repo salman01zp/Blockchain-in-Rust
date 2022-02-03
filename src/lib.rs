dc1bd5a7d1c3cd9209ddc91b3a87d6b78ccbb431
salman01z <pathansalman555@gmail.com>
2022-02-03 19:47:23 +0530

    lib

--- src/lib.rs
+++ src/lib.rs
@@ -79,3 +79,11 @@ pub fn difficulty_bytes_as_u128 (v: &Vec<u8>) -> u128 {
     ((v[16] as u128) << 0x0 * 8)
 }
 
+mod block;
+pub use crate::block::Block;
+mod hashable;
+pub use crate::hashable::Hashable;
+mod blockchain;
+pub use crate::blockchain::Blockchain;
+pub mod transaction;
+pub use crate::transaction::Transaction;
